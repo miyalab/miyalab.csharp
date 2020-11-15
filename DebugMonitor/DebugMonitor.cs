5045c8a38b73c978432d959a2ad628882f2851d6
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2020-11-16 05:38:26 +0900

    Add files via upload

--- DebugMonitor/DebugMonitor.cs
+++ DebugMonitor/DebugMonitor.cs
@@ -9,10 +9,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
+ * 
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
-
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -57,7 +57,8 @@ namespace MiYALAB.CSharp
         public DebugMonitor(int positionX, int positionY)
         {
             InitializeComponent();
-
+            
+            this.Show();
             MoveWindow(positionX, positionY);
         }
 
@@ -71,7 +72,8 @@ namespace MiYALAB.CSharp
         public DebugMonitor(int positionX, int positionY, int sizeX, int sizeY)
         {
             InitializeComponent();
-            
+
+            this.Show();
             MoveWindow(positionX, positionY);
             ChangeWindowSize(sizeX, sizeY);
         }
